846fd19d3dc0221aeeae8b6a9c6702fd67369897
Aliaksandr Karpovich <alexsure.k@gmail.com>
2015-10-14 09:32:41 +0300

    Zhest'

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
     from Compressor import Compressor
-    compressor = Compressor('images/image3.bmp', 8, 8, 100, 200)
+    compressor = Compressor('images/image4.bmp', 8, 8, 100, 200)
     compressor.process()
